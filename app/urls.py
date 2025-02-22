95ee324d40ce4a622fe3645adb682fe0e068048a
FullStackDj <full.stackdj@gmail.com>
2025-02-22 18:13:02 +0200

    The main page is ready. There are index and about

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from main.views import index, about
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('about/', about, name='about'),
+    path('', include('main.urls', namespace='main')),
 ]
--- main/views.py
+++ main/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page - HOME',
+        'title': 'Tea shop - Main',
+        'content': 'Tea shop',
     }
 
     return render(request, 'main/index.html', context)
 
+
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Tes shop - About',
+        'content': 'About us',
+        'text_on_page': "This is a very long text about us",
+
+    }
+    return render(request, 'main/about.html', context)
