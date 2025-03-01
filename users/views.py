1f01203d826414addc9c732f9187799ae84ace7a
FullStackDj <full.stackdj@gmail.com>
2025-03-01 19:46:22 +0200

    Created user application and added necessary templates

---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def login(request):
+    context = {
+        'title': 'Login',
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        'title': 'Registration',
+    }
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    context = {
+        'title': 'Profile',
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    pass
