b6ffd64360d31b6acdd631f8a602b92ef1e08bea
FullStackDj <full.stackdj@gmail.com>
2025-03-04 16:49:04 +0200

    Included all cart templates

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from users import views
+from users.views import login, registration, profile, users_cart, logout
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
-    path('profile/', views.profile, name='profile'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
+    path('users-cart/', users_cart, name='users_cart'),
+    path('logout/', logout, name='logout'),
 ]
