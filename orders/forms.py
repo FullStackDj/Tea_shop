76daeaf284536b4f4c48c68ee835875c645a4170
FullStackDj <full.stackdj@gmail.com>
2025-03-08 17:08:33 +0200

    Created order template and prepared form for placing orders

---
+++ orders/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class CreateOrderForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    phone_number = forms.CharField()
+    required_delivery = forms.CharField()
+    delivery_address = forms.CharField(required=False)
+    payment_on_get = forms.CharField()
+
+
