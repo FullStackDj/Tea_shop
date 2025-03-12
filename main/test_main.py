38f62641d5ae58e05e809dbb128be95f93445ce2
FullStackDj <full.stackdj@gmail.com>
2025-03-12 17:00:39 +0200

    Added tests for the index and about page views

---
+++ main/test_main.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+from .models import Page
+
+
+@pytest.mark.django_db
+def test_index_view(client):
+    Page.objects.create(title='Home', content='Welcome to the home page!')
+    response = client.get(reverse('main:index'))
+    assert response.status_code == 200
+    assert response.context['title'] == 'Home'
+    assert response.context['content'] == 'Welcome to the home page!'
+    assert 'main/index.html' in [t.name for t in response.templates]
+
+
+@pytest.mark.django_db
+def test_about_view(client):
+    Page.objects.create(title='About', content='This is the about page.', text_on_page='Some additional text.')
+    response = client.get(reverse('main:about'))
+    assert response.status_code == 200
+    assert response.context['title'] == 'About'
+    assert response.context['content'] == 'This is the about page.'
+    assert response.context['text_on_page'] == 'Some additional text.'
+    assert 'main/about.html' in [t.name for t in response.templates]
